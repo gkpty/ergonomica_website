54e4ea7321370d181c889a3c398a697cf4975010
gkpty <gkardonski@gmail.com>
2018-12-04 21:11:38 -0500

    fixed product catalog spacing

--- js/ergonomica.js
+++ js/ergonomica.js
@@ -1,4 +1,15 @@
 
+// Floating label headings for the contact form
+  $(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+      $(this).removeClass("floating-label-form-group-with-focus");
+    });
+  });
+
 (function($) {
   "use strict"; // Start of use strict
 
@@ -40,7 +51,6 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);xs
 
-
 })(jQuery); // End of use strict
 
 
@@ -57,5 +67,5 @@ function initMap() {
 }
 
 
-/* Product Selector */
+
 
